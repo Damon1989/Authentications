29f7f7fc9bb383d60299a49409ec20e63bda7084
杨小东 <yangxiaodong@yodoo.net.cn>
2020-09-16 17:31:54 +0800

    ClientCredentials init

---
+++ ClientCredentialsIdentityServer/Config.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace ClientCredentialsIdentityServer
+{
+    public static class Config
+    {
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+        new List<ApiScope>()
+        {
+            new ApiScope("api1","My API")
+        };
+
+        public static IEnumerable<Client> Clients=>
+        new List<Client>()
+        {
+            new Client()
+            {
+                ClientId = "client_id",
+                // secret for authentication
+                ClientSecrets = {new Secret("secret".Sha256())},
+                // no interactive user, use the clientid/secret for authentication
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                // scopes that client has access to 
+                AllowedScopes = {"api1"}
+            }
+        };
+    }
+}
---
+++ IdentityServer/Configuration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IdentityModel;
+using IdentityServer4.Models;
+
+namespace IdentityServer
+{
+    public static class Configuration
+    {
+        public static IEnumerable<ApiResource> GetApis() =>
+            new List<ApiResource>
+            {
+                new ApiResource("ApiOne")
+            };
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("ApiOne","My API")
+            };
+
+        public static IEnumerable<Client> GetClients()=>
+        new List<Client>
+        {
+            new Client()
+            {
+                ClientId = "client_id",
+                ClientSecrets = {new Secret("client_secret".ToSha256())},
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                AllowedScopes = { "ApiOne" }
+            }
+        };
+    }
+}
